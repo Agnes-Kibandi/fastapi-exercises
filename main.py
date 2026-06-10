2c1e1712c60f605da5d94fa09e8b0bb6456476cc
Wangui <kuikibandy@gmail.com>
2026-06-10 01:38:52 -0700

    exe 2 done

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-###**Exercise one.** Create a brand new FastAPI project from scratch. Just `main.py`. No database. No auth. Just the basics. Create three endpoints. A root endpoint that returns your name. A `/hobbies` endpoint that returns a list of your hobbies. A `/bio` endpoint that returns a few facts about yourself.
-
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app=FastAPI()
 
@@ -9,12 +7,17 @@ def name():
     my_name="Agnes"
     return my_name
 
-@app.get("/hobbies")
-def hobbies():
-    my_hobbies=['coding','swimming','fashion','dancing']
-    return my_hobbies
+@app.get("/hobbies/{hobby_id}")
+def hobbies(hobby_id:int):
+    try:
+        my_hobbies=['coding','swimming','fashion','dancing']
+        return my_hobbies[hobby_id]
+    except IndexError:
+        raise HTTPException(status_code=404,detail="no hobby exists")
 
 @app.get("/bio")
 def fun_facts():
     my_fun_facts={'gender':'female','age':37}
     return my_fun_facts
+
+
