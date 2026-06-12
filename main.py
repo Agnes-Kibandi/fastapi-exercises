a4b6af15b63a6ee8329e10a7cda18b73d1f893e6
Wangui <kuikibandy@gmail.com>
2026-06-11 22:44:46 -0700

    exe12 done

--- main.py
+++ main.py
@@ -3,8 +3,10 @@
 from fastapi import FastAPI, HTTPException, Depends
 from typing import Optional
 from pydantic import BaseModel
-from models import BookModel
+from models import BookModel,ReaderModel
 from database import engine,SessionLocal,Base
+from auth import password_checker,password_scrambler,token_generator,token_verifier
+from fastapi.security import OAuth2PasswordRequestForm
 
 Base.metadata.create_all(bind=engine)
 
@@ -19,12 +21,54 @@ class BookOut(BaseModel):
     title:str
     author:str
 
+class ReaderIn(BaseModel):
+    name:str
+    email:str
+    age:int 
+    password:str
+
+class ReaderOut(BaseModel):
+    name:str
+    email:str
+    age:int 
+
+
 def get_db():
     db=SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+
+
+
+@app.post("/reader",response_model=ReaderOut)
+def reader_signup(reader:ReaderIn,db=Depends(get_db)):
+    hashed=password_scrambler(reader.password)
+
+    new_reader=ReaderModel( name=reader.name,email=reader.email,age=reader.age,hashed_password=hashed)
+
+    db.add(new_reader)
+    db.commit()
+    db.refresh(new_reader)
+
+    return new_reader
+
+@app.post("/login")
+def reader_login(form_data:OAuth2PasswordRequestForm= Depends(),db=Depends(get_db)):
+    username=form_data.username
+    password=form_data.password
+
+    user=db.query(ReaderModel).filter(ReaderModel.name==username).first()
+    if user is None:
+        raise HTTPException(status_code=401,detail="invalid credentials")
+    is_valid=password_checker(password,user.hashed_password)
+    if not is_valid:
+        raise HTTPException(status_code=401,detail="invalid credentials")
+    return {"access_token":token_generator({"sub":username}),"token_type":"bearer"}
+
+    
 
 @app.post("/books",response_model=BookOut)
 def save_book(book:Book, db=Depends(get_db)):
